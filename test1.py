723a810a26f4c4f316f36741022ea1810582f33e
poorna1827 <88567369+poorna1827@users.noreply.github.com>
2022-08-03 12:24:40 +0530

    Added new  input feature
    Added a new feature to take start & ending numbers from the user

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
-n = int(input("enter the range : "))
-for i in range(n+1):
+n = int(input("enter starting number : "))
+m = int(input("enter end number : "))
+for i in range(n, m+1):
     if i%2==0:
         print(i , "is even")
     else:
